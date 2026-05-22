cc05fb6363f73db4f6e60d0489f2c5d7097e2ecc
Sumit Singh <sumitsinghr3007@gmail.com>
2026-05-22 21:58:02 +0530

    Fixed cart API crash

--- frontend/src/stores/useUserStore.js
+++ frontend/src/stores/useUserStore.js
@@ -49,7 +49,10 @@ export const useUserStore = create((set, get) => ({
 		set({ checkingAuth: true });
 		try {
 			const response = await axios.get("/auth/profile");
-			set({ user: response.data, checkingAuth: false });
+			set({
+	user: response.data?._id ? response.data : null,
+	checkingAuth: false,
+});
 		} catch (error) {
 			console.log(error.message);
 			set({ checkingAuth: false, user: null });
