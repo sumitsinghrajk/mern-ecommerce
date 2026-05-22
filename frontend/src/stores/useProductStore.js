f70bbac27b6ce3e648be3eb216d1147e95bc8e5d
Sumit Singh <sumitsinghr3007@gmail.com>
2026-05-22 21:46:43 +0530

    Fixed cart API crash

--- frontend/src/stores/useProductStore.js
+++ frontend/src/stores/useProductStore.js
@@ -69,14 +69,18 @@ export const useProductStore = create((set) => ({
 			toast.error(error.response.data.error || "Failed to update product");
 		}
 	},
-	fetchFeaturedProducts: async () => {
-		set({ loading: true });
-		try {
-			const response = await axios.get("/products/featured");
-			set({ products: response.data, loading: false });
-		} catch (error) {
-			set({ error: "Failed to fetch products", loading: false });
-			console.log("Error fetching featured products:", error);
-		}
-	},
+fetchFeaturedProducts: async () => {
+	set({ loading: true });
+	try {
+		const response = await axios.get("/products/featured");
+
+		set({
+			products: Array.isArray(response.data) ? response.data : [],
+			loading: false,
+		});
+	} catch (error) {
+		set({ error: "Failed to fetch products", loading: false });
+		console.log("Error fetching featured products:", error);
+	}
+},
 }));
